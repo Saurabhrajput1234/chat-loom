fa1c62901eb4d2e4ff880bf0bbac63d8ef5e75a7
saurabh <saurabh2113076@akgec.ac.in>
2024-09-24 13:51:38 +0530

    creat home page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,1 +1,17 @@
-cd 
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Chats from "./pages/Chats";
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/chats" element={<Chats />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
